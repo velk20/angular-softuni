be401938f52246170df726be28c36f666054eb96
Angel <48069264+velk20@users.noreply.github.com>
2024-01-28 13:53:38 +0200

    TypeScript

--- 1.Intro to Angular/typescript/src/index.ts
+++ 1.Intro to Angular/typescript/src/index.ts
@@ -1,59 +1,15 @@
-// let username = 'Ivan';
-//
-// username = 1;
-// console.log(username)
+import express from 'express';
 
-let isOpen: boolean = false;
+const app = express();
 
-let arrBoolean: boolean[] = [false, true];
-let arrayBoolean: Array<boolean> = [false, true]; // not so good
+app.get('/',(req,res)=>{
+    res.send('Hello');
+})
 
+app.get('/about',(req,res)=>{
+    res.send('About us');
+})
 
-function createUser(username: string, age?: number) {
-    return{
-        username,
-        age
-    };
-}
-
-const ivan = createUser("ivan", 123);
-const pesho = createUser("pesho");
-
-console.log(ivan);
-console.log(pesho)
-
-
-//! Generics
-function id<T>(item: T): T {
-    return item;
-}
-
-const num = id(1);
-let num1 = id(1);
-
-const str = id('1');
-let prop = id<{prop:string}>({prop: '1'})
-let props = id<{prop:string, prop1:number}>({prop: '1', prop1: 1})
-
-interface IMyDto {
-    prop: string;
-    prop1: number
-}
-
-let obj = id<IMyDto>({prop: "ivan", prop1: 22});
-let obj1 = id<IMyDto|number>({prop: "ivan", prop1: 22});
-
-type MyType = IMyDto | number;
-type BooleanArray = boolean[];
-
-class MyClass implements IMyDto{
-    prop: string;
-    prop1: number;
-
-    constructor(private name: string, private age:number) {
-        this.prop = name;
-        this.prop1 = age;
-    }
-}
-
-const vanko = new MyClass("Vanko", 22);
+app.listen(8080, ()=>{
+    console.log('Server is listening on :8080');
+})
