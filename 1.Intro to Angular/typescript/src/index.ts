d47810923d07253ab22bbe63617595b8559e2282
Angel Mladenov <a.mladenov@seeburger.com>
2024-01-28 13:25:04 +0200

    intro

---
+++ 1.Intro to Angular/typescript/src/index.ts
@@ -0,0 +1,59 @@
+// let username = 'Ivan';
+//
+// username = 1;
+// console.log(username)
+
+let isOpen: boolean = false;
+
+let arrBoolean: boolean[] = [false, true];
+let arrayBoolean: Array<boolean> = [false, true]; // not so good
+
+
+function createUser(username: string, age?: number) {
+    return{
+        username,
+        age
+    };
+}
+
+const ivan = createUser("ivan", 123);
+const pesho = createUser("pesho");
+
+console.log(ivan);
+console.log(pesho)
+
+
+//! Generics
+function id<T>(item: T): T {
+    return item;
+}
+
+const num = id(1);
+let num1 = id(1);
+
+const str = id('1');
+let prop = id<{prop:string}>({prop: '1'})
+let props = id<{prop:string, prop1:number}>({prop: '1', prop1: 1})
+
+interface IMyDto {
+    prop: string;
+    prop1: number
+}
+
+let obj = id<IMyDto>({prop: "ivan", prop1: 22});
+let obj1 = id<IMyDto|number>({prop: "ivan", prop1: 22});
+
+type MyType = IMyDto | number;
+type BooleanArray = boolean[];
+
+class MyClass implements IMyDto{
+    prop: string;
+    prop1: number;
+
+    constructor(private name: string, private age:number) {
+        this.prop = name;
+        this.prop1 = age;
+    }
+}
+
+const vanko = new MyClass("Vanko", 22);
