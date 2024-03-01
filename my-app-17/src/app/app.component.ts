50f94b01fe2c7f301d1b7c836eae69804dfe47eb
Angel <48069264+velk20@users.noreply.github.com>
2024-03-01 18:42:27 +0200

    17 cli

---
+++ my-app-17/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  title = 'my-app-17';
+
+  tasks = [
+    {
+      id: 1,
+      name: 'Finish homerooms',
+      status: 'done',
+    },
+    {
+      id: 2,
+      name: ' nesto si',
+      status: 'todo',
+    },
+  ];
+}
