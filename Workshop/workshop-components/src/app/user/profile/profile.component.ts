034abdc33c969adc5901b8b2c50b5410893e8963
Angel <48069264+velk20@users.noreply.github.com>
2024-03-09 12:29:36 +0200

    profile

--- Workshop/workshop-components/src/app/user/profile/profile.component.ts
+++ Workshop/workshop-components/src/app/user/profile/profile.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {UserService} from "../user.service";
+import {UserForAuth} from "../../types/user";
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+  user: UserForAuth | undefined = {} as UserForAuth;
+  constructor(private userService: UserService) {
+  }
+
+  ngOnInit(): void {
+    this.user = this.userService.user;
+  }
 
 }
