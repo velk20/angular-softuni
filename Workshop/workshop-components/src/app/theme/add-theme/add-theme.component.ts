d8b94dc2b99e8a2b607e5301c7e7288cca1d7c29
Angel <48069264+velk20@users.noreply.github.com>
2024-03-17 21:59:01 +0200

    Email login password validation

--- Workshop/workshop-components/src/app/theme/add-theme/add-theme.component.ts
+++ Workshop/workshop-components/src/app/theme/add-theme/add-theme.component.ts
@@ -1,20 +1,19 @@
 import { Component } from '@angular/core';
-import {ApiService} from "../../api.service";
+import { ApiService } from '../../api.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-add-theme',
   templateUrl: './add-theme.component.html',
-  styleUrls: ['./add-theme.component.css']
+  styleUrls: ['./add-theme.component.css'],
 })
-export class AddThemeComponent{
-    constructor(private apiService: ApiService) {
+export class AddThemeComponent {
+  constructor(private apiService: ApiService) {}
 
+  addTheme(form: NgForm) {
+    if (form.invalid) {
+      return;
     }
-
-  addTheme($event: MouseEvent, themName: string, postText: string) {
-        $event.preventDefault();
-        return this.apiService.createTheme(themName,postText).subscribe((data)=>{
-          console.log(data);
-        })
+    console.log(form.value);
   }
 }
--- Workshop/workshop-components/src/app/theme/theme.module.ts
+++ Workshop/workshop-components/src/app/theme/theme.module.ts
@@ -2,14 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AddThemeComponent } from './add-theme/add-theme.component';
 import { CurrentThemeComponent } from './current-theme/current-theme.component';
-import {ThemesListComponent} from "./themes-list/themes-list.component";
-import {SharedModule} from "../shared/shared.module";
-import {ThemeRoutingModule} from "./theme-routing.module";
-import {PostsListComponent} from "./posts-list/posts-list.component";
-import {MainComponent} from "./main/main.component";
-import {PostItemComponent} from "./posts-list/post-item/post-item.component";
-
-
+import { ThemesListComponent } from './themes-list/themes-list.component';
+import { SharedModule } from '../shared/shared.module';
+import { ThemeRoutingModule } from './theme-routing.module';
+import { PostsListComponent } from './posts-list/posts-list.component';
+import { MainComponent } from './main/main.component';
+import { PostItemComponent } from './posts-list/post-item/post-item.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -18,16 +17,9 @@ import {PostItemComponent} from "./posts-list/post-item/post-item.component";
     ThemesListComponent,
     MainComponent,
     PostsListComponent,
-    PostItemComponent
+    PostItemComponent,
   ],
-  exports: [
-    ThemesListComponent
-  ],
-  imports: [
-    CommonModule,
-    SharedModule,
-    ThemeRoutingModule
-  ]
-
+  exports: [ThemesListComponent],
+  imports: [CommonModule, SharedModule, FormsModule, ThemeRoutingModule],
 })
-export class ThemeModule { }
+export class ThemeModule {}
